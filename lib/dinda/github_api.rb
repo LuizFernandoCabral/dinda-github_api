c12747c29cc31dc9f82e21acd02d154c4a30fde0
Luiz Fernando P. L. Cabral <luizfercabp@gmail.com>
2018-08-29 19:17:10 -0300

    Main module

--- lib/dinda/github_api.rb
+++ lib/dinda/github_api.rb
@@ -1,7 +1,28 @@
-require "dinda/github_api/version"
+require_relative "github_api/version"
+require_relative "github_api/fetcher"
+require_relative "github_api/parser"
 
 module Dinda
   module GithubApi
-    # Your code goes here...
+    def self.perform(repo = 'braspag-rest')
+      commits = Fetcher.fetch_commits(repo)
+
+      commits_per_author = Parser.parse(commits)
+
+      ordered_per_author = commits_per_author.values.sort_by { |commit| commit[:commits_count] }.reverse
+
+      ordered_per_author.map! do |commit|
+        commit.values.map { |value| value ? value:'-'}.join('; ')
+      end
+
+      Dir.mkdir('outputs') unless Dir.exist?('outputs')
+      File.open("outputs/#{repo}-#{Time.now}.txt", 'w+') { |f| f.puts ordered_per_author }
+    end
   end
+end
+
+if ARGV[0]
+  Dinda::GithubApi.perform(ARGV[0])
+else
+  Dinda::GithubApi.perform
 end
